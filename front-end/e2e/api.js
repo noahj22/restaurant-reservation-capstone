132f19cb73fd7b406d68d3e40d01345c0fd7040a
noahj22 <noahjohnson22@gmail.com>
2022-05-09 16:06:29 -0700

    update

--- front-end/e2e/api.js
+++ front-end/e2e/api.js
@@ -1,7 +1,6 @@
 const fetch = require("cross-fetch");
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
