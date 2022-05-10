0a0ba016e14e31a92a0f9673f46839d9d4f3b222
noahj22 <noahjohnson22@gmail.com>
2022-05-09 18:51:24 -0700

    update

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -43,7 +43,6 @@ async function fetchJson(url, options, onCancel) {
    return payload.data;
  } catch (error) {
    if (error.name !== "AbortError") {
-     console.error(error.stack);
      throw error;
    }
    return Promise.resolve(onCancel);
